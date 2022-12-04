1311d5c142af7ea0d7e582d62591abe40dcdc760
Johans Justs Eris <johans.eris@r6vsk.lv>
2022-12-04 19:45:44 +0200

    Rework div grid with css grid

--- main.js
+++ main.js
@@ -1,64 +1,147 @@
-function getRow(){
-    /* Creates a div with a class of 'row'. */
-    
-    const row = document.createElement('div');
-    row.classList.add('row');
-    return row;
-}
-
-function getColumn(){
-    /* Creates a div with a class of 'column'. */
-    
-    const column = document.createElement('div');
-    column.classList.add('column');
-    return column;
-}
+let color = "black";
 
 function getDiv() {
     /* Create a div with a class of 'grid-div'. */ 
     
     const div = document.createElement('div');
     div.classList.add('grid-div');
+    div.style.backgroundColor = "white";
+    // div.style.border = "1px solid red";
     return div;
 }
-
-function createGrid () {
-    /* Creates the 16X16 grid of divs. */ 
-
-    const container = document.querySelector('.container');
-    let counter = 1;
-    for (let i = 1; i <= 16; i++) {
-        row = getRow();
-
-        for (let j = 1; j <= 16; j++) {
-            column = getColumn();
-            div = getDiv();
-
-            // div.textContent = counter++;
-            column.appendChild(div);
-            row.appendChild(column);
-        }
-        container.appendChild(row);
+function changeSize(size) {
+    /* Sets a cusstom size to the canvas. */
+    if (size >= 2 && size <= 100) {
+        deleteBoard();
+        populateBoard(size)
+    }
+    else {
+        console.log("ERORR: Too many squares.");
+        return;
     }
 
-   
 }
 
+function deleteBoard() {
+    /* Deletes existing board. */
+    const board = document.querySelector(".board");
+    // board.textContent = "";
+    let squares = board.querySelectorAll(".grid-div");
+    squares.forEach((div) => div.remove());
+}
 
-function paintGrid(){
-    const divs = document.querySelectorAll('.grid-div');
+function colorSquare() {
+    this.style.backgroundColor = color;
+}
+
+function aaa(square) {
+    // square.addEventListener("mouseover", ()=> {
+    //     square.style.backgroundColor = color;
+
+    // });
+    square.addEventListener("mouseover", colorSquare);
+}
+
+function populateBoard(size = 16) {
+    const board = document.querySelector(".board");
+    let squares = board.querySelectorAll("div");
+    squares.forEach((div) => div.remove());
     
-    divs.forEach((div) => 
-    div.addEventListener(('mouseover'), () => {
-        div.classList.add('coloured');
-    }));
+    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
+    for (let i = 0; i < size*size; i++) {
+        let square = getDiv();
+        aaa(square);
+        square.style.backgroundColor = "white";
+        board.insertAdjacentElement("beforeend", square);
+    }
 }
 
+populateBoard(16)
 
-createGrid();
 
-paintGrid();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // FLEXBOX
+// function getRow(){
+//     /* Creates a div with a class of 'row'. */
+    
+//     const row = document.createElement('div');
+//     row.classList.add('row');
+//     return row;
+// }
+
+// function getColumn(){
+//     /* Creates a div with a class of 'column'. */
+    
+//     const column = document.createElement('div');
+//     column.classList.add('column');
+//     return column;
+// }
+
+// function getDiv() {
+//     /* Create a div with a class of 'grid-div'. */ 
+    
+//     const div = document.createElement('div');
+//     div.classList.add('grid-div');
+//     return div;
+// }
+
+// function changeSize() {
+//     /* Sets a cusstom size to the canvas. */
+    
+//     const size = document.querySelector('#size');
+//     createGrid(size, size);
+// }
+
+// function createGrid (x = 16, y = 16) {
+//     /* Creates the 16X16 grid of divs. */ 
+
+//     const container = document.querySelector('.container');
+//     let counter = 1;
+//     for (let i = 1; i <= y; i++) {
+//         row = getRow();
+
+//         for (let j = 1; j <= x; j++) {
+//             column = getColumn();
+//             div = getDiv();
+
+//             // div.textContent = counter++;
+//             column.appendChild(div);
+//             row.appendChild(column);
+//         }
+//         container.appendChild(row);
+//     }  
+// }
+
+
+// function paintGrid(){
+//     const divs = document.querySelectorAll('.grid-div');
+    
+//     divs.forEach((div) => 
+//     div.addEventListener(('mouseover'), () => {
+//         div.classList.add('coloured');
+//     }));
+
+// }
+
+
+// // createGrid();
+
+// // paintGrid();
 
 
 
